d797a9bcd2089c54ca15565976311c4d4ee444c6
Carlos Quinto <carlos218@revature.net>
2025-01-14 13:23:50 -0800

    book collection controller references

--- TakeItEasy/Controller/Books/BooksViewController.swift
+++ TakeItEasy/Controller/Books/BooksViewController.swift
@@ -7,7 +7,12 @@
 
 import UIKit
 
-class BooksViewController: UIViewController {
+class BooksViewController: UIViewController, UICollectionViewDelegate {
+    
+    @IBOutlet weak var searchBarTitleOrAuthor: UISearchBar!
+    @IBOutlet weak var collectionRecipes: UICollectionView!
+    @IBOutlet weak var collectionTechnical: UICollectionView!
+    @IBOutlet weak var collectionGeneral: UICollectionView!
     
     override func viewDidLoad() {
         super.viewDidLoad()
