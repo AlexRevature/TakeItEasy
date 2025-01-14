99fd6d6ca750f71fb93c58435e24f3f19b930bcb
Carlos Quinto <carlos218@revature.net>
2025-01-14 09:56:51 -0800

    basic books view implemented

---
+++ TakeItEasy/Controller/Books/BooksViewController.swift
@@ -0,0 +1,15 @@
+//
+//  BooksViewController.swift
+//  TakeItEasy
+//
+//  Created by admin on 1/14/25.
+//
+
+import UIKit
+
+class BooksViewController: UIViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+}
