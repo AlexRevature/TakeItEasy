dd4bfad4ddffbba3e6cece2eff0118b582c332ff
Alex Cabrera <180374922+AlexRevature@users.noreply.github.com>
2025-01-24 09:30:24 -0700

    Initial working book list UI - Separate storybaord, remade UI from scratch to be more consistent/cleaner - Still needs more UI work in the future (themeing) - Currently loads information locally, fetching to be set up. - NOTE: Since there isn't anything to be saved anymore, I'm not using CoreData for this. I'll be passing to the PDF Reader a URL to manage.

---
+++ TakeItEasy/Controller/Books/BookListController.swift
@@ -0,0 +1,141 @@
+//
+//  BookListController.swift
+//  TakeItEasy
+//
+//  Created by Alex Cabrera on 1/24/25.
+//
+
+import UIKit
+
+class BookListController: UIViewController {
+
+    @IBOutlet weak var categoryTable: UITableView!
+    var isOnline = false
+
+    var onlineCategoryList = [CategoryInfo(name: "Java"), CategoryInfo(name: "Python"), CategoryInfo(name: "Linux")]
+
+    var offlineCategoryList = [CategoryInfo(name: "Offline", isOnline: false, bookList: [
+        .init(title: "John", author: "John", image: UIImage(systemName: "circle") ?? UIImage(), url: URL(fileURLWithPath: "Test")),
+        .init(title: "John", author: "John", image: UIImage(systemName: "circle") ?? UIImage(), url: URL(fileURLWithPath: "Test")),
+        .init(title: "John", author: "John", image: UIImage(systemName: "circle") ?? UIImage(), url: URL(fileURLWithPath: "Test")),
+    ])]
+
+    func reloadCollection(isOnline: Bool) {
+        self.isOnline = isOnline
+        categoryTable.reloadData()
+    }
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        categoryTable.delegate = self
+        categoryTable.dataSource = self
+    }
+
+}
+
+extension BookListController: UITableViewDelegate, UITableViewDataSource {
+
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return isOnline ? onlineCategoryList.count : offlineCategoryList.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "CategoryCell", for: indexPath) as! CategoryCell
+        let currentCategory = isOnline ? onlineCategoryList[indexPath.row] : offlineCategoryList[indexPath.row]
+
+        cell.categoryLabel.text = currentCategory.name
+
+        cell.booksCollection.delegate = self
+        cell.booksCollection.dataSource = self
+        cell.booksCollection.categoryIndex = indexPath.row
+        cell.booksCollection.reloadData()
+
+        return cell
+    }
+}
+
+extension BookListController: UICollectionViewDelegate, UICollectionViewDataSource {
+
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+
+        guard let booksCollection = collectionView as? BooksCollection else {
+            return 0
+        }
+
+        guard let categoryIndex = booksCollection.categoryIndex else {
+            return 0
+        }
+
+        return isOnline ? onlineCategoryList[categoryIndex].bookList.count : offlineCategoryList[categoryIndex].bookList.count
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+
+        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "BookCell", for: indexPath) as! BookCell
+
+        guard let booksCollection = collectionView as? BooksCollection else {
+            return cell
+        }
+
+        guard let categoryIndex = booksCollection.categoryIndex else {
+            return cell
+        }
+
+        let currentBook = isOnline ? onlineCategoryList[categoryIndex].bookList[indexPath.row] : offlineCategoryList[categoryIndex].bookList[indexPath.row]
+
+        cell.backView.backgroundColor = ThemeManager.lightTheme.backColor
+        cell.backView.layer.borderColor = UIColor.systemBackground.cgColor
+        cell.backView.layer.borderWidth = 0.2
+
+        cell.backView.layer.cornerRadius = 18.0
+        cell.backView.layer.masksToBounds = false
+
+        cell.backView.layer.shadowColor = UIColor.black.cgColor
+        cell.backView.layer.shadowOpacity = 0.5
+        cell.backView.layer.shadowOffset = CGSize(width: 4, height: 4)
+        cell.backView.layer.shadowRadius = 4
+
+        cell.backView.layer.shadowPath = UIBezierPath(
+            roundedRect: cell.backView.bounds,
+            cornerRadius: cell.backView.layer.cornerRadius
+        ).cgPath
+
+        cell.titleLabel.text = currentBook.title
+        cell.authorLabel.text = currentBook.author
+        cell.imageHolder.image = currentBook.image
+        cell.imageHolder.backgroundColor = UIColor.gray
+
+        return cell
+    }
+
+}
+
+struct BookInfo {
+    var title: String
+    var author: String
+    var image: UIImage
+    var url: URL
+}
+
+struct CategoryInfo {
+    var name: String
+    var isOnline = true
+    var bookList = [BookInfo]()
+}
+
+class CategoryCell: UITableViewCell {
+    @IBOutlet weak var backView: UIView!
+    @IBOutlet weak var categoryLabel: UILabel!
+    @IBOutlet weak var booksCollection: BooksCollection!
+}
+
+class BookCell: UICollectionViewCell {
+    @IBOutlet weak var backView: UIView!
+    @IBOutlet weak var titleLabel: UILabel!
+    @IBOutlet weak var authorLabel: UILabel!
+    @IBOutlet weak var imageHolder: UIImageView!
+}
+
+class BooksCollection: UICollectionView {
+    var categoryIndex: Int?
+}
