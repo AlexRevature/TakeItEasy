2de170fe80d0544b1c7460e42411105f7351d079
Joshua Hatch <jshhatch@gmail.com>
2025-01-30 11:54:47 -0500

    fixed the bug. the note datat array was gettin emptied and then the search method would automatically kick you out becasue of that. NOw the sarrary is never emptied so you're always seraching form th efull note list

--- TakeItEasy/Controller/Notes/NotesViewController.swift
+++ TakeItEasy/Controller/Notes/NotesViewController.swift
@@ -121,16 +121,12 @@ class NotesViewController:  UIViewController, UITableViewDelegate, UITableViewDa
     func searchBar(_ searchBar: UISearchBar, textDidChange searchText: String) {
         var temporaryArray : [StoredNote] = []
         
-        if (noteData.isEmpty) {
-            return
-        }
-        
         if (searchText == "") {
             reloadFromCoreData()
             return
         }
         
-        //seraches by title
+        //searches by title
         for note in noteData {
             let noteName = note.name!.lowercased()
             if noteName.contains(searchText.lowercased()) {
@@ -151,51 +147,14 @@ class NotesViewController:  UIViewController, UITableViewDelegate, UITableViewDa
         
         noteData = temporaryArray
         tableView.reloadData()
+        refreshSearchPool()
     }
     
-    /*
-    // Override to support conditional editing of the table view.
-    override func tableView(_ tableView: UITableView, canEditRowAt indexPath: IndexPath) -> Bool {
-        // Return false if you do not want the specified item to be editable.
-        return true
+    func refreshSearchPool() {
+        let notesFromCoreData = UserManager.getNoteList()
+        if  notesFromCoreData != nil {
+            noteData = UserManager.getNoteList()!
+        }
+        sortNotesByRecentcy()
     }
-    */
-
-    /*
-    // Override to support editing the table view.
-    override func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCell.EditingStyle, forRowAt indexPath: IndexPath) {
-        if editingStyle == .delete {
-            // Delete the row from the data source
-            tableView.deleteRows(at: [indexPath], with: .fade)
-        } else if editingStyle == .insert {
-            // Create a new instance of the appropriate class, insert it into the array, and add a new row to the table view
-        }    
-    }
-    */
-
-    /*
-    // Override to support rearranging the table view.
-    override func tableView(_ tableView: UITableView, moveRowAt fromIndexPath: IndexPath, to: IndexPath) {
-
-    }
-    */
-
-    /*
-    // Override to support conditional rearranging of the table view.
-    override func tableView(_ tableView: UITableView, canMoveRowAt indexPath: IndexPath) -> Bool {
-        // Return false if you do not want the item to be re-orderable.
-        return true
-    }
-    */
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
-    }
-    */
-
 }
